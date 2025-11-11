120354b3a8f450e70b1543e6c04e88b7606e9d35
choehyeonjin <guswls2616@naver.com>
2025-11-11 20:55:48 +0900

    Feat: week07 member review API 응답 통일

--- src/main/java/com/example/umc9th/domain/review/controller/ReviewQueryController.java
+++ src/main/java/com/example/umc9th/domain/review/controller/ReviewQueryController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.review.controller;
 
 import com.example.umc9th.domain.review.dto.ReviewResponse;
 import com.example.umc9th.domain.review.service.ReviewQueryService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,18 @@ public class ReviewQueryController {
     private final ReviewQueryService reviewQueryService;
 
     @GetMapping
-    public List<ReviewResponse> myReviews(
+    public ApiResponse<List<ReviewResponse>> myReviews(
             @PathVariable Long memberId,
             @RequestParam(required = false) String storeName,
             @RequestParam(required = false) Integer ratingBand
     ) {
-        return reviewQueryService.getMyReviews(memberId, storeName, ratingBand);
+        // 응답 코드 정의
+        GeneralSuccessCode code = GeneralSuccessCode.OK;
+
+        // 응답 데이터 정의
+        List<ReviewResponse> data = reviewQueryService.getMyReviews(memberId, storeName, ratingBand);
+
+        // code+message, result 응답
+        return ApiResponse.onSuccess(code, data);
     }
 }
